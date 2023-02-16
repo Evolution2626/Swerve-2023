7c71374d0d75cd62eb08d84d897efb7e545c19ae
Zach3292 <zach3292@hotmail.com>
2023-02-16 08:44:01 -0500

    fixed the speed

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -37,9 +37,9 @@ public class SwerveDriveCommand extends CommandBase {
     speedY = Range.threshold(0.1, speedY);
     speedR = Range.threshold(0.1, speedR);
 
-    speedX *= 3;
-    speedY *= 3;
-    speedR *= 3;
+    speedX *= 10;
+    speedY *= 10;
+    speedR *= 15;
 
     drivetrain.driveSwerve(speedX, -speedY, speedR);
   }
